1c983a84887d3508ccc1872ddb7dd69a34e5e4b2
Alex Walczyk <abwalczyk@gmail.com>
2025-10-16 10:39:38 -0400

    Adding cert trust to control

--- setup-automation/setup-control.sh
+++ setup-automation/setup-control.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+set -e
 
+# Trust Satellite CA and register system
+curl -k -L https://${SATELLITE_URL}/pub/katello-server-ca.crt -o /etc/pki/ca-trust/source/anchors/${SATELLITE_URL}.ca.crt
+update-ca-trust
+rpm -Uhv https://${SATELLITE_URL}/pub/katello-ca-consumer-latest.noarch.rpm || true
+
+subscription-manager status >/dev/null 2>&1 || \
+  subscription-manager register --org=${SATELLITE_ORG} --activationkey=${SATELLITE_ACTIVATIONKEY} --force
+
+# Allow passwordless sudo for rhel user
 echo "%rhel ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/rhel_sudoers
 chmod 440 /etc/sudoers.d/rhel_sudoers
 
-sudo -u rhel install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-10.noarch.rpm
-sudo -u rhel install -y certbot
+# Install Certbot via Satellite repos
+sudo dnf install -y certbot || \
+  (sudo dnf install -y epel-release && sudo dnf install -y certbot)
