7bf31868e7a0e3c481e72ae836cd73f21d779298
Alex Walczyk <abwalczyk@gmail.com>
2025-10-26 08:55:49 -0400

    Fixing api endpoint to get token in challenge 3

--- runtime-automation/module-03/setup-vscode.sh
+++ runtime-automation/module-03/setup-vscode.sh
@@ -1,7 +1,11 @@
-RUNAS="sudo -u rhel"
+#!/bin/bash
+set -e
 
-#Runs bash with commands between '_' as nobody if possible
-$RUNAS bash<<_
+# Ensure rhel user exists
+id rhel >/dev/null 2>&1 || useradd -m rhel
+
+# Run commands as rhel
+sudo -u rhel bash <<'EOF_RHEL'
 mkdir /home/rhel/minimal-downstream-with-hub/
 mkdir /home/rhel/minimal-downstream-with-hub/files/
 
@@ -52,19 +56,19 @@ build_arg_defaults:
 
 EOF
 
-token=`curl -s -u admin:ansible123! -H "Content-Type: application/json" -X POST control.lab -k | jq .token`
+token=`curl -s -u admin:ansible123! -H "Content-Type: application/json" -X POST control.lab/api/galaxy/v3/auth/token/ -k | jq .token`
 
 cat <<EOF >> /home/rhel/minimal-downstream-with-hub/files/ansible.cfg
 [galaxy]
 server_list = validated_repo,rh_certified_repo
 
 [galaxy_server.validated_repo]
-url=https://privatehub-01.${guid}.instruqt.io/api/galaxy/content/validated/
-token=`curl -s -u admin:ansible123! -H "Content-Type: application/json" -X POST control.lab -k | jq .token | xargs`
+url=https://control-${GUID}.apps.ocpvdev01.rhdp.net/api/galaxy/content/validated/
+token=`curl -s -u admin:ansible123! -H "Content-Type: application/json" -X POST control.lab/api/galaxy/v3/auth/token/ -k | jq .token | xargs`
 
 [galaxy_server.rh_certified_repo]
-url=https://privatehub-01.${guid}.instruqt.io/api/galaxy/content/rh-certified/
-token=`curl -s -u admin:ansible123! -H "Content-Type: application/json" -X POST control.lab -k | jq .token | xargs`
+url=https://control-${GUID}.apps.ocpvdev01.rhdp.net/api/galaxy/content/rh-certified/
+token=`curl -s -u admin:ansible123! -H "Content-Type: application/json" -X POST control.lab/api/galaxy/v3/auth/token/ -k | jq .token | xargs`
 
 EOF
 _
