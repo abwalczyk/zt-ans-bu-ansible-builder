3d4506069089ddb498f4787e50c3fd6cbc39bdfa
Alex Walczyk <abwalczyk@gmail.com>
2025-10-26 09:26:18 -0400

    Removing _ at end of file

--- runtime-automation/module-03/setup-vscode.sh
+++ runtime-automation/module-03/setup-vscode.sh
@@ -6,10 +6,11 @@ id rhel >/dev/null 2>&1 || useradd -m rhel
 
 # Run commands as rhel
 sudo -u rhel bash <<'EOF_RHEL'
-mkdir /home/rhel/minimal-downstream-with-hub/
-mkdir /home/rhel/minimal-downstream-with-hub/files/
+mkdir -p /home/rhel/minimal-downstream-with-hub/files/
+mkdir -p /home/rhel/minimal-downstream-with-hub/solution-definition/
 
-cat > /home/rhel/minimal-downstream-with-hub/execution-environment.yml << EOF
+# --- Base EE config
+cat > /home/rhel/minimal-downstream-with-hub/execution-environment.yml <<EOF
 ---
 version: 3
 
@@ -26,8 +27,8 @@ options:
   package_manager_path: /usr/bin/microdnf
 EOF
 
-mkdir /home/rhel/minimal-downstream-with-hub/solution-definition/
-cat > /home/rhel/minimal-downstream-with-hub/solution-definition/execution-environment.yml << EOF
+# --- Solution definition EE config
+cat > /home/rhel/minimal-downstream-with-hub/solution-definition/execution-environment.yml <<EOF
 ---
 version: 3
 
@@ -38,7 +39,7 @@ images:
 dependencies:
   galaxy:
     collections:
-    - ansible.netcommon
+      - ansible.netcommon
 
 options:
   package_manager_path: /usr/bin/microdnf
@@ -53,22 +54,22 @@ additional_build_steps:
 
 build_arg_defaults:
   ANSIBLE_GALAXY_CLI_COLLECTION_OPTS: '--ignore-certs'
-
 EOF
 
-token=`curl -s -u admin:ansible123! -H "Content-Type: application/json" -X POST control.lab/api/galaxy/v3/auth/token/ -k | jq .token`
+# --- Get token
+token=$(curl -s -u admin:ansible123! -H "Content-Type: application/json" \
+  -X POST https://control.lab/api/galaxy/v3/auth/token/ -k | jq -r .token)
 
-cat <<EOF >> /home/rhel/minimal-downstream-with-hub/files/ansible.cfg
+# --- Write ansible.cfg
+cat > /home/rhel/minimal-downstream-with-hub/files/ansible.cfg <<EOF
 [galaxy]
 server_list = validated_repo,rh_certified_repo
 
 [galaxy_server.validated_repo]
 url=https://control-${GUID}.apps.ocpvdev01.rhdp.net/api/galaxy/content/validated/
-token=`curl -s -u admin:ansible123! -H "Content-Type: application/json" -X POST control.lab/api/galaxy/v3/auth/token/ -k | jq .token | xargs`
+token=${token}
 
 [galaxy_server.rh_certified_repo]
 url=https://control-${GUID}.apps.ocpvdev01.rhdp.net/api/galaxy/content/rh-certified/
-token=`curl -s -u admin:ansible123! -H "Content-Type: application/json" -X POST control.lab/api/galaxy/v3/auth/token/ -k | jq .token | xargs`
-
-EOF
-_
+token=${token}
+EOF_RHEL
