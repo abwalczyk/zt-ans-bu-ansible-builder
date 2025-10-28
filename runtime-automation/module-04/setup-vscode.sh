0a6624c08036930ce3dfd1e8bd95e6a3d344cc31
Alex Walczyk <abwalczyk@gmail.com>
2025-10-28 13:40:43 -0400

    Testing

--- runtime-automation/module-04/setup-vscode.sh
+++ runtime-automation/module-04/setup-vscode.sh
@@ -88,21 +88,4 @@ token=${token}
 EOF
 curl https://letsencrypt.org/certs/lets-encrypt-r3.pem --output /home/rhel/minimal-downstream-with-hub-certs/files/cert.pem
 
-touch /etc/sudoers.d/rhel_sudoers
-echo "%rhel ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/rhel_sudoers
-cp -a /root/.ssh/* /home/rhel/.ssh/.
-chown -R rhel:rhel /home/rhel/.ssh
-#dnf config-manager --enable rhui*
-
-sudo systemctl stop pulpcore-api
-sudo systemctl stop nginx
-sudo systemctl start snapd
-sudo certbot certonly --no-bootstrap --standalone -d control-${GUID}.${DOMAIN} --email ansible-network@redhat.com --noninteractive --agree-tos
-sudo cp /etc/letsencrypt/live/control-${GUID}.${DOMAIN}/privkey.pem /etc/pulp/certs/pulp_webserver.key
-sudo cp /etc/letsencrypt/live/control-${GUID}.${DOMAIN}/fullchain.pem /etc/pulp/certs/pulp_webserver.crt
-sudo restorecon -v /etc/pulp/certs/pulp_webserver.crt
-sudo restorecon -v /etc/pulp/certs/pulp_webserver.key
-sudo systemctl start pulpcore-api
-sudo systemctl start nginx
-
 EOF_RHEL
